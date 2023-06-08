b39658ee421c7df3d4d103ddf0f7ed5388ed1e84
Akram Ali <akram.exp02@gmail.com>
2023-06-08 12:29:52 +0530

    changes

--- main.tf
+++ main.tf
@@ -38,12 +38,18 @@ data "civo_ssh_key" "ssh-key" {
   name = "ssh-key"
 }
 
+data "civo_firewall" "my-instance-firewall" {
+    name = "my-instance-firewall"
+    region = "LON1"
+}
+
 resource "civo_instance" "server" {
     hostname = "server"
     notes = "this is a note for the server"
     size = element(data.civo_instances_size.extra-small.sizes, 0).name
     disk_image = element(data.civo_disk_image.ubuntu.diskimages, 0).id
     sshkey_id = data.civo_ssh_key.ssh-key.id
+    firewall_id = data.civo_firewall.my-instance-firewall.id
 }
 
 output "server_ip" {
